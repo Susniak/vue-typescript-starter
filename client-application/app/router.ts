05aae297e1b04a527cef7e043c15f68d96786f14
msuniak <marek.susniak@oath.com>
2018-03-20 21:52:03 +0100

    Basement webpack configuration

---
+++ client-application/app/router.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+
+const routes: RouteConfig[] = [];
+export const router = new VueRouter({ routes });
+Vue.use(VueRouter);
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const build = require('path').resolve(__dirname, 'build');
+
+module.exports = {
+    entry: './client-application/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 9000,
+        noInfo: true,
+        overlay: true,
+        historyApiFallback: true,
+        hot: true,
+        open: true
+    },
+    output: {
+        filename: 'build.js',
+        publicPath: '/build/',
+        path: build
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        scss: 'vue-style-loader!css-loader!sass-loader',
+                        sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.vue', '.json'],
+        alias: {
+            vue$: 'vue/dist/vue.esm.js'
+        }
+    }
+};
